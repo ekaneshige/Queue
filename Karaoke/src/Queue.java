14b4bb39433f98d3d224bb10f71a7087a37b1fa4
Eric Kaneshige <etkanesh@ucsc.edu>
2014-12-23 04:20:51 -0700

    First Push of starting code

---
+++ Karaoke/src/Queue.java
@@ -0,0 +1,26 @@
+
+public class Queue {
+
+	/**
+	 * @param args
+	 */
+	public static void protocol() {
+		List newList = new List();
+		//await for input from user from gui
+		//add song to the list.
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		protocol();
+		// TODO Auto-generated method stub
+		
+		//Create LinkedList Data Structure Object
+		//Create Protocol Object or Function
+		//Create GUI that responds to the linked list
+		
+	}
+	
+
+}
